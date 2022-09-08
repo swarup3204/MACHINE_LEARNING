f06eebfbc40d99426229d0fa772443c12abbf764
Grce47 <gracesharma47@gmail.com>
2022-09-08 12:53:44 +0530

    changed q1.py

--- q1.py
+++ q1.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
-from sklearn import model_selection
 
+# for random test/train split
+def test_train_split(db,train_size=0.3) -> tuple:
+    random_suffled = db.iloc[np.random.permutation(len(db))]
+    split_point = int(len(db)*0.3)
+    return random_suffled[:split_point].reset_index(drop=True),random_suffled[split_point:].reset_index(drop=True)
 
 dataset = pd.read_csv("Train_B_Tree.csv")
 
-train, test = model_selection.train_test_split(dataset,test_size=0.3)
-print(train)
+train, test = test_train_split(dataset,test_size=0.3)
